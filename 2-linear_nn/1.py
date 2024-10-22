184d35fbc4b287393a0e26a6fc13983ccd2b09fd
Alann9 <3047228490@qq.com>
2024-10-22 20:07:50 +0800

    first commit

---
+++ 2-linear_nn/1.py
@@ -0,0 +1,3 @@
+import numpy as np
+y_hat = np.array([[0.1, 0.3, 0.6], [0.3, 0.2, 0.5]])
+print(y_hat[range(len(y_hat))])
---
+++ 2-linear_nn/datasets.py
@@ -0,0 +1,16 @@
+import torch
+import torchvision
+from torch.utils import data
+from torchvision import transforms
+from d2l import torch as d2l
+from matplotlib import pyplot as plt
+
+
+
+if __name__ == '__main__':
+    train_iter, test_iter = d2l.load_data_fashion_mnist(32)
+    for X, y in train_iter:
+        print(X.shape, X.dtype, y.shape, y.dtype)
+        break
+    d2l.show_images(X.reshape(32, 28, 28), 2, 9, titles=d2l.get_fashion_mnist_labels(y))
+    plt.show()

7c00509ac8bc97f1f4317ff8ea9c74a2a3fa8a4f
Alann9 <3047228490@qq.com>
2024-10-22 23:57:49 +0800

    fix bug

--- 2-linear_nn/datasets.py
+++ 2-linear_nn/datasets.py
@@ -3,8 +3,11 @@ import torchvision
 from torch.utils import data
 from torchvision import transforms
 from d2l import torch as d2l
-//
+
 from matplotlib import pyplot as plt
+
+
+
 
 if __name__ == '__main__':
     train_iter, test_iter = d2l.load_data_fashion_mnist(32)
